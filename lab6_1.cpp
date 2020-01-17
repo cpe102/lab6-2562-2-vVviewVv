fd50d2535a117eba4b5c764b38261769cd9e480c
vVviewVv <viewissara1246@gmail.com>
2020-01-17 16:56:59 +0700

    d

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 int main() {
+    
     int a  =  20,b = 30;
     double c = 4,output;
 
     output = (double) a-b/c;
     cout << "output:" << output << "\n";
     return 0;
-    
+
 }
